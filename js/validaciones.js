2bfb757b05eb4d4f78ef8460a7ac8c0c91fd3cc2
Dolapelao <101908344+Dolapelao@users.noreply.github.com>
2023-05-11 21:28:18 -0400

    Add files via upload

---
+++ js/validaciones.js
@@ -0,0 +1,207 @@
+function validarNombre(nombre)
+{
+    if(nombre.length >= 3 && nombre.length <= 20)
+    {
+        return true;
+    }
+    return false;
+}
+
+function validarPassword(password)
+{
+    if(password.length >= 3 && password.length <= 20)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarRut(rut)
+{
+    if(rut.length >= 8 && rut.length <= 10)
+    {
+        return true;
+    }
+    return false;
+}
+function validarApellidoPa(apellido_pa)
+{
+    if(apellido_pa.length >= 3 && apellido_pa.length <= 20)
+    {
+        return true;
+    }
+    return false;
+}
+function validarApellidoMa(apellido_ma)
+{
+    if(apellido_ma.length >= 3 && apellido_ma.length <= 20)
+    {
+        return true;
+    }
+    return false;
+}
+function validarEdad(edad)
+{
+    if(edad >= 18 && edad <= 150)
+    {
+        return true;
+    }
+    return false;
+}
+function validarCelular(celular)
+{
+    if(celular.length >= 8 && celular.length <= 11)
+    {
+        return true;
+    }
+    return false;
+}
+function validarCorreo(correo)
+{
+    if(correo.length >= 3 && correo.length <= 20)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarTipo(tipo)
+{
+    if(tipo != "")
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarCodAt(cod_at)
+{
+    if(cod_at.length >= 1 && cod_at.length <= 3)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarCodAd(cod_ad)
+{
+    if(cod_ad.length >= 1 && cod_ad.length <= 3)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarRechazo(rechazo)
+{
+    if(rechazo != "")
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarAtencion(atencion)
+{
+    if(atencion != "")
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarMarca(marca)
+{
+    if(marca.length >= 3 && marca.length <= 20)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarModelo(modelo)
+{
+    if(modelo.length >= 3 && modelo.length <= 20)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarPassword(password)
+{
+    if(password.length >= 3 && password.length <= 20)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarVehiculo(vehiculo)
+{
+    if(vehiculo != "")
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarObservacion(observacion)
+{
+    if(observacion.length >= 3 && observacion.length <= 500)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarObs(obs)
+{
+    if(obs.length >= 3 && obs.length <= 500)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarDireccion(direccion)
+{
+    if(direccion.length >= 3 && direccion.length <= 50)
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarFechaNac(fecha_nac)
+{
+    if(fecha_nac != "")
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarFechaAte(fecha_ate)
+{
+    if(fecha_ate != "")
+    {
+        return true;
+    }
+    return false;
+
+}
+function validarImagen1(imagen1)
+{
+    if(imagen1 != "")
+    {
+        return true;
+    }
+    return false;
+
+}
+
+/* comentarios */
+
+
+
